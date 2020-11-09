8a930e7af459764358d3acb687dbbb19323310e7
c17an <kimchanmin1@gmail.com>
2020-11-09 20:45:25 +0900

    날씨 컴포넌트 추가

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./Weather.scss";
 
 function Weather() {
-  return <div>날씨</div>;
+  const [currentIcon, setCurrentIcon] = useState("");
+  const [currentTemp, setCurrentTemp] = useState("");
+  const [perceptionTemp, setPerceptionTemp] = useState("");
+  const [weeklyIcon, setWeeklyIcon] = useState([]);
+  const [weeklyTemp, setWeeklyTemp] = useState([]);
+
+  useEffect(() => {
+    fetch("https://calm-mesa-43659.herokuapp.com/weather")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setCurrentIcon(data.currentIcon);
+        setCurrentTemp(data.currentTemp);
+        setPerceptionTemp(data.perceptionTemp);
+        setWeeklyIcon(data.weeklyIcon);
+        setWeeklyTemp(data.weeklyTemp);
+      });
+  }, []);
+  return (
+    <div className="Weather">
+      <div className="Weather__title">고양시 화전동</div>
+      <section className="Weather__icon">
+        <img src={currentIcon} alt="weather" width="70"></img>
+      </section>
+      <section className="Weather__temp">
+        <div>기온</div>
+        <span style={{ display: "inline-block", marginTop: "8px" }}>
+          {parseFloat(currentTemp).toFixed(1)}°C
+        </span>
+      </section>
+      <section className="Weather__perception">
+        <div>체감온도</div>
+        <span style={{ display: "inline-block", marginTop: "8px" }}>
+          {parseFloat(perceptionTemp).toFixed(1)}°C
+        </span>
+      </section>
+    </div>
+  );
 }
 
 export default Weather;
