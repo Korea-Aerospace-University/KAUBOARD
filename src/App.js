667f7bf58028b1703d915597efe7119369377d0c
c17an <kimchanmin1@gmail.com>
2020-11-12 12:38:30 +0900

    배경 이미지 슬라이드 구현

--- src/App.js
+++ src/App.js
@@ -3,12 +3,21 @@ import Footer from "./views/Footer/Footer";
 import Main from "./views/Main/Main";
 import Top from "./views/Top/Top";
 import "./App.scss";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [backgroundIndex, setBackgroundIndex] = useState(1);
+  useEffect(() => {
+    let id = setInterval(() => {
+      setBackgroundIndex(Math.floor((Math.random() * 100) % 17));
+      return clearInterval(id);
+    }, 5000);
+  }, [backgroundIndex]);
+
   const styles = {
     background: {
-      backgroundImage: `url(./background1.jpg)`,
-      backgroundPosition: "0 20",
+      backgroundImage: `url(./background${backgroundIndex}.jpg)`,
+      //backgroundPosition: "0 20",
       backgroundRepeat: "no-repeat",
       backgroundSize: "cover",
     },
@@ -17,7 +26,7 @@ function App() {
   return (
     <div className="App" style={styles.background}>
       <div className="Wrapper">
-        <Top></Top>
+        {/* <Top></Top> */}
         <Main></Main>
         <Footer></Footer>
       </div>
