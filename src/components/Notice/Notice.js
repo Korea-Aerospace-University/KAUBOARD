73e1d7ad1b079b6890e0377ef2079b693b35cac0
c17an <kimchanmin1@gmail.com>
2020-11-17 01:41:13 +0900

    공지사항 데이터 연동

--- src/components/Notice/Notice.js
+++ src/components/Notice/Notice.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Notice.scss";
 
 function Notice() {
+  useEffect(() => {
+    fetch("https://calm-mesa-43659.herokuapp.com/notification")
+      .then((res) => res.json())
+      .then((data) => {
+        setGeneralNotiList(data.generalNotiList.slice(0, 4));
+        setSchoolNotiList(data.schoolNotiList.slice(0, 4));
+        setIsLoading(false);
+      });
+  }, []);
+  const [generalNotiList, setGeneralNotiList] = useState([]);
+  const [schoolNotiList, setSchoolNotiList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const isNewItem = (item) => {
+    console.log(new Date(item.date).getDate(), new Date().getDate());
+    if (
+      new Date(item.date).getDate() === new Date().getDate() &&
+      new Date(item.date).getMonth() === new Date().getMonth()
+    ) {
+      return "new";
+    } else {
+      return "old";
+    }
+  };
+
   return (
     <div className="Notice">
       <nav className="Notice__general">
@@ -13,14 +38,13 @@ function Notice() {
         </div>
         <div className="Notice__border"></div>
         <div className="Notice__container">
-          <article>
-            [항공대학군단] 제 50, 51기 학군사관후보생 최종합격자 공지
-          </article>
-          <article>
-            2020학년도 제2학기 재학생 및 연구등록 등록기간 및 방법 안내
-          </article>
-          <article>힐링 집단상담 실시 [학생마일리지10시간]</article>
-          <article>2020 블루밍 총여학생회 장학금(생활비성) 신청 안내</article>
+          {isLoading
+            ? "로딩 중..."
+            : generalNotiList.map((item) => (
+                <article className={`article__${isNewItem(item)}`}>
+                  {item.content}
+                </article>
+              ))}
         </div>
       </nav>
       <nav className="Notice__academy">
@@ -32,10 +56,13 @@ function Notice() {
         </div>
         <div className="Notice__border"></div>
         <div className="Notice__container">
-          <article>2020학년도 동계 계절학기 실시 안내</article>
-          <article>조기 취업학생 수업 대체인정 시행 안내 (변경)</article>
-          <article>학부모학사정보서비스 실시 안내</article>
-          <article>유고결석 인정 기타 사유 세부기준 및 신청 절차 안내</article>
+          {isLoading
+            ? "로딩 중..."
+            : schoolNotiList.map((item) => (
+                <article className={`article__${isNewItem(item)}`}>
+                  {item.content}
+                </article>
+              ))}
         </div>
       </nav>
     </div>
