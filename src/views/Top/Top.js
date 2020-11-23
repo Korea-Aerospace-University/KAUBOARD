15df3e5e284b4130832fca9437db15a4bd902303
c17an <kimchanmin1@gmail.com>
2020-11-23 23:05:23 +0900

    탑 뷰 불필요한 컴포넌트 삭제

--- src/views/Top/Top.js
+++ src/views/Top/Top.js
@@ -1,8 +1,6 @@
 import React from "react";
 import Clock from "../../components/Clock/Clock";
-import MealMenu from "../../components/MealMenu/MealMenu";
 import Notice from "../../components/Notice/Notice";
-import Weather from "../../components/Weather/Weather";
 import "./Top.scss";
 
 function Top() {
