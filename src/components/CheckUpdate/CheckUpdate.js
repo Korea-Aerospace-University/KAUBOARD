dda6a8c000d48417847679c7c969f4fef941ac89
c17an <kimchanmin1@gmail.com>
2020-11-23 23:03:07 +0900

    업데이트 확인 컴포넌트 추가

---
+++ src/components/CheckUpdate/CheckUpdate.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import "./CheckUpdate.scss";
+
+// 업데이트 시마다 무조건 바꿔주기!!
+const version = "1.0.0";
+
+function CheckUpdate() {
+  const [versionState, setVersionState] = useState(null);
+  const [backColor, setBackColor] = useState("#ffffff");
+  const [checking, setChecking] = useState(false);
+  const onUpdateBtnClick = () => {
+    setChecking(true);
+    fetch("https://calm-mesa-43659.herokuapp.com/checkVersion")
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.currentVersion === version) {
+          setVersionState("현재 최신 버전입니다.");
+          setBackColor("#3b42a5");
+        } else {
+          setVersionState("업데이트가 필요합니다.");
+          setBackColor("#b94949");
+        }
+        setChecking(false);
+      });
+  };
+  return (
+    <div onClick={onUpdateBtnClick}>
+      {/* 버전 정보가 있으면 텍스트 출력 */}
+      {versionState ? (
+        <div
+          className="CheckUpdate__status"
+          style={{ backgroundColor: backColor }}
+        >
+          {versionState}
+        </div>
+      ) : // 버전 정보 없으면 아이콘 출력
+      checking ? (
+        <img className="CheckUpdate__icon" src="./checkUpdate.svg"></img>
+      ) : (
+        <img
+          className="CheckUpdate__icon"
+          src="update.png"
+          alt="업데이트 확인"
+          style={{ backgroundColor: "white" }}
+        ></img>
+      )}
+    </div>
+  );
+}
+
+export default CheckUpdate;
